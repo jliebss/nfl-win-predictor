835d23a8e9b2db0dcae2391bf8cda7d52f9289b1
Joel Liebert <joelmliebert@gmail.com>
2022-04-25 12:34:20 -0400

    knn is giving 97% accuracy over 23 years. no idea how that's possible, but I'm committing now so I dont't lose what's happening

--- nfl.R
+++ nfl.R
@@ -33,11 +33,11 @@ library(iml)
 
 
 # ---- Load Data ----
-raw_data <- nflfastR::load_pbp(2020:2021)
+raw_data <- nflfastR::load_pbp(1999:2021)
 raw_data <- clean_pbp(raw_data)
 raw_data <- raw_data[complete.cases(raw_data$posteam), ]
 
-schedule <- fast_scraper_schedules(2020:2021) %>% 
+schedule <- fast_scraper_schedules(1999:2021) %>% 
   mutate(winner_name = case_when(home_result > 0 ~ home_team,
                                  home_result < 0 ~ away_team,
                                  TRUE            ~ "TIE"))
@@ -50,9 +50,6 @@ season_stats <- raw_data %>%
                sum, # make sure this doesn't break
                na.rm = TRUE) %>% 
   merge(schedule[, c("game_id", "season", "winner_name", "home_team", "away_team")], by = "game_id", all.x = TRUE)
-# %>% 
-#   mutate(winner = factor(case_when(posteam == winner_name ~ 1,
-#                                    TRUE                   ~ 0)))
 
 cumulative_stats <- season_stats %>% 
   group_by(posteam, season) %>% 
@@ -92,17 +89,24 @@ model_stats <- home_team_stats %>%
   merge(away_team_stats, by = c("game_id", "winner_name")) %>% 
   mutate(winner = factor(case_when(posteam.x == winner_name ~ 1,
                                    posteam.y == winner_name ~ 0))) %>% 
-  dplyr::select(-c(winner_name, posteam.x, posteam.y))
+  dplyr::select(-c(winner_name, posteam.x, posteam.y)) %>% 
+  drop_na()
 
 set.seed(123)
 smp_size  <- floor(0.75 * nrow(model_stats))
 train_ind <- sample(seq_len(nrow(model_stats)), size = smp_size)
 
-train <- model_stats[train_ind,  -c(1, ncol(model_stats)-1)]
-test  <- model_stats[-train_ind, -c(1, ncol(model_stats)-1)]
+train_not_scaled <- model_stats[train_ind,  -1] %>% 
+  drop_na()
+test_not_scaled  <- model_stats[-train_ind, -1] %>% 
+  drop_na()
 
-# ---- Logistic Regression Models ----
-# Base case
+train <- train_not_scaled
+test  <- test_not_scaled
+train[, -ncol(train)] <- scale(train[, -ncol(train)])
+test[, -ncol(test)]   <- scale(test[, -ncol(test)])
+
+# ---- Logistic Regression ----
 logit_fit   <- glm(winner ~ .,
                  data   = train,
                  family = binomial)
@@ -112,116 +116,83 @@ logit_probs <- predict(logit_fit,
 logit_pred  <- ifelse(logit_probs > 0.5, 1, 0)
 
 logit_table <- table(test$winner, logit_pred)
-logit_cm    <- confusionMatrix(logit_table)
+logit_cm    <- caret::confusionMatrix(logit_table)
 logit_acc   <- logit_cm$overall[1]
 
-# 
-# # Forward
-# glm_fit_forward <- stepAIC(glm_fit,
-#                             direction = "both",
-#                             trace = FALSE)
-# glm_probs_forward <- predict(glm_fit_forward,
-#                               newdata = test[, -ncol(test)],
-#                               type = "response")
-# glm_pred_forward <- ifelse(glm_probs_forward > 0.5, 1, 0)
-# pred_forward <- sum(glm_pred_forward == test$winner) / length(glm_pred_forward)
-# 
-# 
-# # Backward
-# glm_fit_backward <- stepAIC(glm_fit,
-#                             direction = "both",
-#                             trace = FALSE)
-# glm_probs_backward <- predict(glm_fit_backward,
-#                               newdata = test[, -ncol(test)],
-#                               type = "response")
-# glm_pred_backward <- ifelse(glm_probs_backward > 0.5, 1, 0)
-# pred_backward <- sum(glm_pred_backward == test$winner) / length(glm_pred_backward)
-# 
-# 
-# # Both
-# glm_fit_both <- stepAIC(glm_fit,
-#                         direction = "both",
-#                         trace = FALSE)
-# glm_probs_both <- predict(glm_fit_both,
-#                           newdata = test[, -ncol(test)],
-#                           type = "response")
-# glm_pred_both <- ifelse(glm_probs_both > 0.5, 1, 0)
-# pred_both <- sum(glm_pred_both == test$winner) / length(glm_pred_both)
-# 
-# 
-# # ---- Test Models
-# summary(glm_fit)
-# summary(glm_fit_backward)
-# summary(glm_fit_forward)
-# summary(glm_fit_both)
-# 
-# results_df <- data_frame(model    = c("base", "forward", "backward", "both"),
-#                          accuracy = c(pred_base, pred_forward, pred_backward, pred_both))
-# view(results_df)
-# 
-# 
-# log_base_cm <- table(test$winner, logit_pred)
-# acc <- confusionMatrix(log_base_cm)
-# acc$overall[1]
 
-# ---- Naive Bayes Models ----
+# ---- Naive Bayes ----
 naive_model <- naiveBayes(winner ~ .,
-                          data = train_scaled)
+                          data = train)
 
 naive_pred  <- predict(naive_model,
-                       newdata = test_scaled)
+                       newdata = test)
 
-naive_table <- table(test_scaled$winner, naive_pred)
-naive_cm    <- confusionMatrix(naive_table)
+naive_table <- table(test$winner, naive_pred)
+naive_cm    <- caret::confusionMatrix(naive_table)
 naive_acc   <- naive_cm$overall[1]
 
 
 # ---- KNN ----
-knn_pred <- knn(train = train_scaled[, -ncol(train_scaled)],
-                test  = test_scaled[, -ncol(test_scaled)],
-                cl    = train_scaled[,  ncol(train_scaled)],
+knn_pred <- knn(train = train,
+                test  = test,
+                cl    = train$winner,
                 k     = 11)
 
-knn_table <- table(test_scaled$winner, knn_pred)
-knn_cm    <- confusionMatrix(knn_table)
+knn_table <- table(test$winner, knn_pred)
+knn_cm    <- caret::confusionMatrix(knn_table)
 knn_acc   <- knn_cm$overall[1]
 
 
 # ---- Support Vector Machine ----
 svm_model <- svm(formula = winner ~ .,
-                 data    = train_scaled)
+                 data    = train)
 
 svm_pred  <- predict(svm_model,
-                     newdata = test_scaled[, -ncol(test_scaled)])
+                     newdata = test)
 
-svm_table <- table(test_scaled$winner, svm_pred)
-svm_cm    <- confusionMatrix(svm_table)
+svm_table <- table(test$winner, svm_pred)
+svm_cm    <- caret::confusionMatrix(svm_table)
 svm_acc   <- svm_cm$overall[1]
 
 
 # ---- Decision Tree ----
 tree_model <- rpart(formula = winner ~ .,
-                    data    = train_scaled,
+                    data    = train,
                     cp = 0.07444)
 
 tree_pred <- predict(tree_model,
-                     newdata = test_scaled[, -ncol(test_scaled)],
+                     newdata = test,
                      type = 'class')
 
-tree_table <- table(test_scaled$winner, tree_pred)
-tree_cm    <- confusionMatrix(tree_table)
+tree_table <- table(test$winner, tree_pred)
+tree_cm    <- caret::confusionMatrix(tree_table)
 tree_acc   <- tree_cm$overall[1]
+
+
+# ---- Vegas ----
+vegas_results_df <- model_stats[-train_ind, ] %>% 
+  merge(raw_data[, c("game_id", "spread_line")], by = "game_id", all.x = TRUE) %>% 
+  drop_na() %>% 
+  distinct() %>% 
+  mutate(spread = factor(case_when(spread_line < 0 ~ 0,
+                                   spread_line > 0 ~ 1)))
+
+
+vegas_table <- table(test$winner, vegas_results_df$spread)
+vegas_cm    <- caret::confusionMatrix(vegas_table)
+vegas_acc   <- vegas_cm$overall[1]
+
 
 # ---- Model Comparison ----
 results_df <-
-  data.frame(model    = c("logistic", "naive_bayes", "knn", "svm", "decision_tree"),
-             accuracy = c(logit_acc, naive_acc, knn_acc, svm_acc, tree_acc)) %>%
+  data.frame(model    = c("logistic", "naive_bayes", "knn", "svm", "decision_tree", "vegas"),
+             accuracy = c(logit_acc, naive_acc, knn_acc, svm_acc, tree_acc, vegas_acc)) %>%
   arrange(desc(accuracy))
-
+view(results_df)
 
 # ---- IML ----
-features <- as.data.frame(box_score[, !names(box_score) %in% c("game_id", "posteam", "winner_name", "winner")])
-response <- as.numeric(as.vector(box_score$winner))
+features <- as.data.frame(model_stats[, !names(model_stats) %in% c("game_id", "winner")])
+response <- as.numeric(as.vector(model_stats$winner))
 pred <- function(model, newdata) {
   results <- as.data.frame(predict(model, newdata, type = "response"))
   return(results)
@@ -251,17 +222,14 @@ interact.glm +
   theme_classic() +
   ggtitle("Logistic Regression Feature Interaction")
 
-interact.ry <- Interaction$new(predictor.glm, feature = "receiving_yards_name") %>% 
+interact.ry <- Interaction$new(predictor.glm, feature = "yards_gained.x") %>% 
   plot()
 interact.ry +
   theme_classic() +
   ggtitle("Logistic Regression Feature Interaction with Receiving Yards")
 
 # Shapley Values
-
-# ptm <- proc.time()
-
-high  <- predict(logit_fit, features) %>% 
+high <- predict(logit_fit, features) %>% 
   as.vector() %>% 
   which.max()
 
@@ -271,17 +239,4 @@ shapley.glm <- Shapley$new(predictor.glm, x.interest = highest_prob_win)
 sum(shapley.glm$results["phi"])
 plot(shapley.glm) +
   theme_classic()
-+
-  ggtitle("Shapley Values for TEN vs JAX (42-20)")
-
-
-proc.time() - ptm
-
-
-
-
-
-
-
-
 
